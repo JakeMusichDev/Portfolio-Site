93dcace9c8362272afabcfeb11622844955e6025
JakeMusichDev <jake@the-artery.com>
2018-05-24 10:31:09 -0400

    throwaway

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,8 @@ import Scroller from '../components/home-components/Scroller'
 import TitleHeader from '../components/general-components/TitleHeader'
 
 import backgroundImg from '../../assets/home/home_bk.jpeg'
+import displacementImg from '../../assets/home/displacementMapTile.jpg'
+
 import * as PIXI from 'pixi.js'
 
 export default class Home extends Component {
@@ -14,27 +16,58 @@ export default class Home extends Component {
   componentDidMount() {
     this.initPixi()
   }
+
+
   render() {
     let component = this;
     return (
-      <div id='home' className={css(styles.homeContainer)}>
+      <div className={css(styles.homeContainer)}>
         <div ref={(thisDiv) => {component.canvasAnchor = thisDiv}} />
         <Scroller />
       </div>
     )
   }
 
+
+
   initPixi = () => {
-    //Aliases
-    const home = document.getElementById('home')
+    this.setupApp()
 
-    let Application = PIXI.Application,
-        loader = PIXI.loader,
-        resources = PIXI.loader.resources,
-        Sprite = PIXI.Sprite;
+    var displacementSprite = PIXI.Sprite.fromImage("http://i.imgur.com/2yYayZk.png")
 
+    //load an image and run the `setup` function when it's done
+    PIXI.loader
+      .add(`${backgroundImg}`)
+      .on("progress", loadProgressHandler)
+      .load(setup);
+
+    const progressHandler = function() {
+      console.log("loading");
+    }
+
+    var displacementSprite = PIXI.Sprite.fromImage(`${displacementImg}`);
+    var displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+    displacementSprite.scale.y = 0.1;
+    displacementSprite.scale.x = 0.1;
+
+    function setup() {
+      const back = new Sprite(resources[`${backgroundImg}`].texture)
+      back.height = window.innerHeight
+      back.width = window.innerWidth
+
+      back.filters = [new PIXI.filters.BlurFilter()]
+
+      this.app.stage.filters = [displacementFilter]
+      this.app.stage.addChild(displacementFilter)
+      this.app.stage.addChild(back)
+    }
+
+  };
+
+  setupApp = () => {
     //Create a Pixi Application
-    let app = new Application({
+    this.app = new PIXI.Application({
         width: window.innerWidth,
         height: window.innerHeight,
         antialias: true,
@@ -43,38 +76,22 @@ export default class Home extends Component {
       }
     );
 
-    //Add the canvas that Pixi automatically created for you to the HTML document
-    this.canvasAnchor.appendChild(app.view);
+    //Add the canvas that Pixi automatically created for you to the canvas container
+    this.canvasAnchor.appendChild(this.app.view);
+  };
 
-    //load an image and run the `setup` function when it's done
-    loader
-      .add(`${backgroundImg}`)
-      .on("progress", loadProgressHandler)
-      .load(setup);
+  setImg = () => {
+    const back = new Sprite(resources[`${backgroundImg}`].texture)
+    back.height = window.innerHeight
+    back.width = window.innerWidth
 
-      function loadProgressHandler() {
-        console.log("loading");
-      }
-
-    //This `setup` function will run when the image has loaded
-    function setup() {
-
-      //Create the cat sprite
-      const back = new Sprite(resources[`${backgroundImg}`].texture);
-      back.height = window.innerHeight
-      back.width = window.innerWidth
-
-
-      // const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-      back.filters = [new PIXI.filters.BlurFilter()];
-
-      // app.stage.filters = [displacementFilter];
-      // app.stage.addChild(displacementFilter);
-      // app.stage.addChild(image);
-      app.stage.addChild(back);
-    }
-
+    this.app.stage.filters = [displacementFilter]
+    this.app.stage.addChild(displacementFilter)
+    this.app.stage.addChild(back)
   }
+
+
+
 }
 
 const styles = StyleSheet.create({
@@ -87,3 +104,76 @@ const styles = StyleSheet.create({
     height: '200vh',
   },
 })
+
+
+
+
+// var width = window.offsetWidth;
+// var height = window.offsetHeight;
+// var playground = document.getElementById('px-render');
+//
+// var canvas;
+//
+// var ratio = 150 / 830;
+//
+// var count = 0;
+// var raf;
+//
+//
+// var renderer = PIXI.autoDetectRenderer(width, height, {transparent:true});
+// renderer.autoResize = true;
+// var tp, preview;
+// var displacementSprite,
+// displacementFilter,
+// stage;
+//
+// function setScene(url){
+//       playground.appendChild(renderer.view);
+//
+//         stage = new PIXI.Container();
+//
+//         tp = PIXI.Texture.fromImage(url);
+//         preview = new PIXI.Sprite(tp);
+//
+//         preview.anchor.x = 0;
+//
+//         displacementSprite = PIXI.Sprite.fromImage('https://res.cloudinary.com/dvxikybyi/image/upload/v1486634113/2yYayZk_vqsyzx.png');
+//         displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+//
+//         displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+//
+//         displacementSprite.scale.y = 0.6;
+//         displacementSprite.scale.x = 0.6;
+//
+//
+//         stage.addChild(displacementSprite);
+//
+//         stage.addChild(preview);
+//
+//     animate();
+// }
+//
+// function removeScene(){
+// cancelAnimationFrame(raf);
+// stage.removeChildren();
+// stage.destroy(true);
+// playground.removeChild(canvas);
+// }
+//
+//
+// function animate() {
+//   raf = requestAnimationFrame(animate);
+//
+//   displacementSprite.x = count*10;
+// displacementSprite.y = count*10;
+//
+// count += 0.05;
+//
+//   stage.filters = [displacementFilter];
+//
+//   renderer.render(stage);
+//
+//   canvas = playground.querySelector('canvas');
+// }
+//
+// setScene('https://unsplash.it/600/?random');
